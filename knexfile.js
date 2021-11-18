b5bfb17180a2b4584fac218804bc9a1567c749db
jordanbmowry <jordan.mowry@gmail.com>
2021-11-18 14:05:20 -0700

    created /src/errors and made four error handler files in folder also reconfigured knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,46 +1,52 @@
-const path = require("path");
+const path = require('path');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  NODE_ENV = 'development',
+  DEVELOPMENT_DATABASE_URL,
+  PRODUCTION_DATABASE_URL,
 } = process.env;
+const URL =
+  NODE_ENV === 'production'
+    ? PRODUCTION_DATABASE_URL
+    : DEVELOPMENT_DATABASE_URL;
 
 module.exports = {
   development: {
-    client: "postgresql",
-    connection: DATABASE_URL,
+    client: 'postgresql',
+    connection: URL,
     pool: { min: 0, max: 5 },
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: path.join(__dirname, 'src', 'db', 'seeds'),
     },
   },
 
   production: {
-    client: "postgresql",
-    connection: DATABASE_URL,
+    client: 'postgresql',
+    connection: URL,
     pool: { min: 0, max: 5 },
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: path.join(__dirname, 'src', 'db', 'seeds'),
     },
   },
 
   test: {
-    client: "sqlite3",
+    client: 'sqlite3',
     connection: {
-      filename: ":memory:",
+      filename: ':memory:',
     },
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: path.join(__dirname, 'src', 'db', 'seeds'),
     },
     useNullAsDefault: true,
   },
--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
-if (process.env.USER) require("dotenv").config();
-const express = require("express");
+if (process.env.USER) require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+app.use(cors());
 const app = express();
 
 module.exports = app;
