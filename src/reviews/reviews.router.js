0c40f4ea071c9521abe1de01f91bdaefbf582afa
jordanbmowry <jordan.mowry@gmail.com>
2021-11-21 09:54:23 -0700

    Added controller, service, and router file to handle DELETE /reviews/:reviewId and PUT /reviews/:reviewId.

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const controller = require('./reviews.controller');
 const methodNotAllowed = require('../errors/methodNotAllowed');
-
-router.route('/:reviewId').delete(controller.delete).all(methodNotAllowed);
+//DELETE, PUT /reviews/:reviewId
+router
+  .route('/:reviewId')
+  .delete(controller.delete)
+  .put(controller.update)
+  .all(methodNotAllowed);
 
 module.exports = router;
