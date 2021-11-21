0cef95fe03ffc28e98daab1b6045f90e66dcb7e2
jordanbmowry <jordan.mowry@gmail.com>
2021-11-21 09:57:41 -0700

    Added additional routes to app.js.

--- src/app.js
+++ src/app.js
@@ -6,15 +6,20 @@ const notFound = require('./errors/notFound');
 const errorHandler = require('./errors/errorHandler');
 const moviesRouter = require('./movies/movies.router');
 const reviewsRouter = require('./reviews/reviews.router');
+const theatersRouter = require('./theaters/theaters.router');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 app.use('/movies', moviesRouter);
 app.use('/reviews', reviewsRouter);
+// app.use('/theaters', theatersRouter);
 
 app.use(notFound);
 
 app.use(errorHandler);
 
 module.exports = app;
+
+// https://github.com/Josh-Bowman/BackendCapstone-Movies
